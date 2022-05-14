6cccf3bd2bff13414935908d0314fcd9a99372f8
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-14 12:58:41 -0300

    add gitignore

---
+++ ConverterTemp/ConverterTemp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterTemp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f, k;
+
+            Console.WriteLine("Conversor de temperaturas");
+            Console.Write("Insira a temperatura em C°: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------------");
+
+            f = (c * 9 / 5) + 32;
+
+            k = c + 273.15;
+
+            Console.WriteLine(c + " graus celcius = " + f + " graus fahrenheit");
+            Console.WriteLine(c + " graus celcius = " + k + " graus kelvin");
+
+            Console.WriteLine("---------------------------");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EntradaESaida/EntradaESaida/InverterNomes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaESaida
+{
+    internal class InverterNomes
+    {
+        static void Main(string[] args)
+        {
+            string nome1, nome2, nome3, nome4, aux;
+
+            Console.Write("Digite o nome 1: ");
+            nome1 = Console.ReadLine();
+
+            Console.Write("Digite o nome 2: ");
+            nome2 = Console.ReadLine();
+
+            Console.Write("Digite o nome 3: ");
+            nome3 = Console.ReadLine();
+
+            Console.Write("Digite o nome 4: ");
+            nome4 = Console.ReadLine();
+
+            // mecanismo para inverter os nomes
+
+            aux = nome1;
+            nome1 = nome4;
+            nome4 = aux;
+            aux = nome2;
+            nome2 = nome3;
+            nome3 = aux;
+
+            Console.WriteLine();
+            Console.WriteLine("Nomes inseridos na sequencia invertida");
+            Console.WriteLine(nome1);
+            Console.WriteLine(nome2);
+            Console.WriteLine(nome3);
+            Console.WriteLine(nome4);
+
+            Console.ReadKey();
+        }
+    }
+}
