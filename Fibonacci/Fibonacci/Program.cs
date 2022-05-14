edef7204419974d0c630e1325f94efd6469a0c3b
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-14 13:45:10 -0300

    Update Program.cs

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -10,6 +10,28 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            // 0 1 1 2 3 5 8 ...
+            // soma dos dois anteriores
+            int a = 0, b = 1, c = 0;
+            Console.Write("Quantos valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sequencia de Fibonacci com " + valores + " valores");
+
+            for (int i = 0; i < valores; i++)
+            {
+                if (i < valores - 1)
+                    Console.Write(a + ", ");
+                else
+                    Console.Write(a);
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
