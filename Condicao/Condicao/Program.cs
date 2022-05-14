ad9eb8d14df5cb807901c1d815ed046f26b6e8a8
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-14 12:44:54 -0300

    condições if simples

---
+++ Condicao/Condicao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Condição simples
+            int valor = 7;
+
+            if (valor < 5)
+            {
+                Console.WriteLine("Condição verdadeira");
+            }
+            else
+            {
+                Console.WriteLine("Condição retornou falso");
+            }
+            #endregion
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
