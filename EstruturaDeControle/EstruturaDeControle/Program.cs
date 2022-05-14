716727c414ef459f98d3a0229f9d129d6bcd4ae5
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-14 13:16:04 -0300

    Update Program.cs

--- EstruturaDeControle/EstruturaDeControle/Program.cs
+++ EstruturaDeControle/EstruturaDeControle/Program.cs
@@ -10,24 +10,49 @@ namespace EstruturaDeControle
     {
         static void Main(string[] args)
         {
-            int escolha = 1;
+            #region Switch - case
+            //int escolha = 1;
 
-            switch(escolha)
+            //switch(escolha)
+            //{
+            //    case 1:
+            //        Console.WriteLine("opção 1");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("opção 2");
+            //        break;
+            //    case 3:
+            //    case 4:
+            //        Console.WriteLine("opção combinada");
+            //        break;
+            //    default:
+            //        Console.WriteLine("opção default");
+            //        break;
+            //}
+            #endregion
+
+            #region goto
+
+            Inicio:
+            Console.Write("Escolha uma opção: ");
+            int op = int.Parse(Console.ReadLine());
+            int valor = 0;
+
+            switch(op)
             {
                 case 1:
-                    Console.WriteLine("opção 1");
+                    valor += 100;
                     break;
                 case 2:
-                    Console.WriteLine("opção 2");
-                    break;
-                case 3:
-                case 4:
-                    Console.WriteLine("opção combinada");
-                    break;
+                    valor += 50;
+                    goto case 1;
                 default:
-                    Console.WriteLine("opção default");
-                    break;
+                    goto Inicio;
             }
+
+            Console.WriteLine("Valor final: " + valor);
+
+            #endregion
 
             Console.ReadKey();
         }
